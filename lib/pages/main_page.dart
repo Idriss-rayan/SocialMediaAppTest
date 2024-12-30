6f217421488b0ad4d14e2cd5f30587dcb691af26
Idriss-rayan <rayanidriss27@gmail.com>
2024-12-30 23:44:52 +0600

    new add

--- lib/pages/main_page.dart
+++ lib/pages/main_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:newproject/pages/home_page.dart';
 import 'package:newproject/pages/profile_page.dart';
+import 'package:newproject/styles/app_colors.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -16,44 +17,46 @@ class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // appBar: AppBar(
-      //   title: Text("Bottom navigation container"),
-      // ),
       body: pages[currentIndex],
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset('assets/svg/home.svg'),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset('assets/svg/favorite.svg'),
-            label: 'Favorite',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset('assets/svg/mess.svg'),
-            label: 'Add Post',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset('assets/svg/mess.svg'),
-            label: 'Messages',
-          ),
-          BottomNavigationBarItem(
-            icon: SvgPicture.asset('assets/svg/user.svg'),
-            label: 'Users',
-          ),
-        ],
-        currentIndex: currentIndex,
-        onTap: (index) {
-          setState(() {
-            currentIndex = index;
-          });
-        },
-        type: BottomNavigationBarType.fixed,
-        showSelectedLabels: false,
-        showUnselectedLabels: false,
-        backgroundColor: Colors.amber,
-      ),
+      bottomNavigationBar: MyBottomNavigation()
+//       BottomNavigationBar(
+//         items: [
+//           BottomNavigationBarItem(
+//             icon: SvgPicture.asset('assets/svg/home.svg'),
+//             label: 'Home',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: SvgPicture.asset('assets/svg/favorite.svg'),
+//             label: 'Favorite',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: Icon(
+//                 Icons.add,
+//               size: 24,
+//               color: Colors.black,
+//             ),
+//             label: 'Add Post',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: SvgPicture.asset('assets/svg/mess.svg'),
+//             label: 'Messages',
+//           ),
+//           BottomNavigationBarItem(
+//             icon: SvgPicture.asset('assets/svg/user.svg'),
+//             label: 'Users',
+//           ),
+//         ],
+//         currentIndex: currentIndex,
+//         onTap: (index) {
+//           setState(() {
+//             currentIndex = index;
+//           });
+//         },
+//         type: BottomNavigationBarType.fixed,
+//         showSelectedLabels: false,
+//         showUnselectedLabels: false,
+//         backgroundColor: Colors.amber,
+//   ),
     );
   }
 
@@ -71,3 +74,64 @@ class _MainPageState extends State<MainPage> {
     ProfilePage(),
   ];
 }
+
+
+
+class MyBottomNavigation extends StatefulWidget {
+  const MyBottomNavigation({super.key});
+
+  @override
+  State<MyBottomNavigation> createState() => _MyBottomNavigationState();
+}
+
+class _MyBottomNavigationState extends State<MyBottomNavigation> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 87,
+      margin: EdgeInsets.all(24),
+      child: Stack(
+        children: [
+          Positioned(
+            right: 0,
+            left: 0,
+            top: 17,
+            child: Container(
+              height: 70,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(25)),
+              ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            top: 0,
+            child: Container(
+              width: 64,
+              height: 64,
+              decoration: BoxDecoration(
+                color: AppColors.primary,
+                shape: BoxShape.circle,
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
